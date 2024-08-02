babd63680747e9da604819818da7cf4a4b99cd9a
tornike.despotashvili@gmail.com <tornike.despotashvili@gmail.com>
2024-08-02 20:26:13 +0400

    update setting panel

--- src/pages/Doctors/DocCard.tsx
+++ src/pages/Doctors/DocCard.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import FlexibleButton from '../../components/buttons/FlexibleButton';
 import { DeleteDoctor } from './DeleteDoctor';
 
@@ -8,16 +8,25 @@ interface DocProps {
     avatar: string;
     workExp: number;
     profile: string;
+    index: number;
+    activeIndex: number | null;
+    handlePanelVisibility: (index: number) => void;
 }
 
-const DocCard: React.FC<DocProps> = ({ id, avatar, name, profile, workExp }) => {
-
-    const { docDeleteHandler } = DeleteDoctor()
+const DocCard: React.FC<DocProps> = ({ id, avatar, name, profile, workExp, index, activeIndex, handlePanelVisibility }) => {
+    const { docDeleteHandler } = DeleteDoctor();
 
     return (
         <div className="doc_card" key={id}>
-            <div className='setting_dots_style' onClick={()=>docDeleteHandler(id)}>
-                <p>. . .</p>
+            <div className='setting_dots_style'>
+                <p onClick={() => handlePanelVisibility(index)}>. . .</p>
+
+                {activeIndex === index && (
+                    <div className='setting_panel'>
+                        <p className='setting_pannel_paragraph'>Edit Doctor</p>
+                        <p className='setting_pannel_paragraph del' onClick={() => docDeleteHandler(id)}>Delete Doctor</p>
+                    </div>
+                )}
             </div>
             <img src={avatar} alt="doc image" className="doc_avatar" />
 
@@ -29,8 +38,7 @@ const DocCard: React.FC<DocProps> = ({ id, avatar, name, profile, workExp }) => 
 
             <FlexibleButton btnText={'Book now'} btnHeight={30} />
         </div>
-    )
-}
+    );
+};
 
-export default DocCard
-
+export default DocCard;
--- src/pages/Doctors/Doctors.tsx
+++ src/pages/Doctors/Doctors.tsx
@@ -1,26 +1,40 @@
 import "./doctors.css";
 import AddNewDoctor from "./AddNewDoctor";
 import DocCard from "./DocCard";
-import { Key } from "react";
 import { UseDoctorDataBaseHook } from "../../hooks/UseDoctorDataBase";
+import { useState } from "react";
 
 
 const Doctors = () => {
     const { docDb } = UseDoctorDataBaseHook()
+
+    const [activeIndex, setActiveIndex] = useState<number | null>(null);
+
+    const handlePanelVisibility = (clickedIndex: number) => {
+        if (clickedIndex === activeIndex) {
+            setActiveIndex(null);
+        } else {
+            setActiveIndex(clickedIndex); 
+        }
+    };
+
 
     return (
         <div className="doctors_main_page">
             <AddNewDoctor />
             <div className="doctor_map">
                 {docDb.length > 0 ? (
-                    docDb.map((doc, index: Key) => (
+                    docDb.map((doc, index: number) => (
                         <DocCard
+                            activeIndex={activeIndex}
+                            index={index}
                             key={index}
                             id={doc.id}
                             name={doc.name}
                             avatar={doc.avatar}
                             workExp={doc.workExp}
                             profile={doc.profile}
+                            handlePanelVisibility={handlePanelVisibility}
                         />
                     ))
                 ) : (
